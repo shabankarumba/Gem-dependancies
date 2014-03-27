6f1a49b6354c65ca158c230d93409851c5c36213
Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-03-27 12:00:18 +0000

    It now retrieves gem dependancies

--- lib/gem_dependancy.rb
+++ lib/gem_dependancy.rb
@@ -4,30 +4,41 @@ require 'json'
 class GemDependancy
   include HTTParty
   
-  attr_accessor :gem_name, :runtime_dependancy, :gem_json
+  attr_accessor :runtime_dependancy, :gem_json
 
-  def initialize(gem_name)
-    @gem_name = gem_name
+  def initialize
     @runtime_dependancy = []
     @gem_json = ""
   end
 
-  def retrieve_gem_dependancies
+  def retrieve_gem_dependancies(gem_name)
     begin
-      response = HTTParty.get("http://rubygems.org/api/v1/gems/#{@gem_name}.json")
+      response = HTTParty.get("http://rubygems.org/api/v1/gems/#{gem_name}.json")
       @gem_json = JSON.parse(response.body)
+      puts "Ruby Gem name:- #{gem_name}"
     rescue JSON::ParserError
       "The gem your are trying to find dependencies for does not exist"
     end
   end
 
   def retrieve_runtime_dependancies
-    @gem_json[0]["dependencies"]["runtime"].each do |dependencies|
+    gem_data = @gem_json.is_a?(Array) ? @gem_json[0] : @gem_json
+    gem_data["dependencies"]["runtime"].each do |dependencies|
       @runtime_dependancy << dependencies['name']
     end
   end
 
   def retrive_dependancies
-    retrieve_gem_dependancies
+    @runtime_dependancy.each do |dependency|
+      retrieve_gem_dependancies(dependency)
+      retrieve_runtime_dependancies
+      output_dependancies 
+    end
+  end
+
+  def output_dependancies
+    @runtime_dependancy.each do |name|
+      puts "Runtime dependancy:- #{name}" 
+    end
   end
 end
--- spec/lib/gem_dependancy_spec.rb
+++ spec/lib/gem_dependancy_spec.rb
@@ -9,28 +9,18 @@ describe GemDependancy do
       "requirements"=> "= 4.0.4"}]} }]
   end
 
-  subject do
-   GemDependancy.new("activerecord")
-  end
-  
-  describe "#retrieve_dependancies" do
-    it "retrieves the gems dependancies" do
-      subject.gem_name.should == "activerecord"
-    end
-  end
-
-  describe "#retrieve_gem_dependancies" do
-
+  describe "#retrieve_gem_dependancies success" do
     it "returns the gem if it exists" do
       response = double("Response", body: '[ {"name"=>"activerecord", "downloads"=>30367907, "version"=>"4.0.4"}]' )
       HTTParty.should_receive(:get).and_return(response)
-      subject.retrieve_gem_dependancies
+      subject.retrieve_gem_dependancies("activerecord")
     end
+  end
 
+  describe "#retrieve_gem_dependancies failure" do
     it "returns an error if  the gem does not exist" do
-      subject.gem_name = "activerecordrrr"
       subject.stub(retrieve_gem_dependancies: "The gem your are trying to find dependencies for does not exist" )
-      subject.retrieve_gem_dependancies.should == "The gem your are trying to find dependencies for does not exist"
+      subject.retrieve_gem_dependancies("activerecordrrr").should == "The gem your are trying to find dependencies for does not exist"
     end
   end
 
@@ -42,6 +32,35 @@ describe GemDependancy do
     it "returns the runtime dependancies " do
       subject.retrieve_runtime_dependancies
       subject.runtime_dependancy.should == [ "activemodel" ]
+    end
+  end
+
+  describe "#retrieves_dependancies" do
+    before do
+      subject.runtime_dependancy = ["activemodel"]
+    end
+
+    it "retrieves the dependancies" do
+      subject.stub(retrive_dependancies: "Ruby Gem name:- activemodel \n
+                        Runtime dependancy:- activemodel \n
+                        Runtime dependancy:- activesupport \n
+                        Runtime dependancy:- builder\n ")
+      subject.retrive_dependancies.should eq "Ruby Gem name:- activemodel \n
+                        Runtime dependancy:- activemodel \n
+                        Runtime dependancy:- activesupport \n
+                        Runtime dependancy:- builder\n "
+    end
+  end
+
+  describe "#output_dependancies" do
+    before do
+      subject.runtime_dependancy = ["activemodel", "two"]
+    end
+
+    it "outputs the name of the dependancy" do
+      subject.should_receive(:puts).with("Runtime dependancy:- activemodel")
+      subject.should_receive(:puts).with("Runtime dependancy:- two")
+      subject.output_dependancies
     end
   end
 end
